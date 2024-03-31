da7406214f9ff5c782261ab736f8875d58830b6c
BikramThangjam <tbsingh1211@gmail.com>
2024-03-31 20:05:56 +0530

    add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
-function login(username: string): boolean {
-    return true;
+function login(username: string): string {
+    const user = {
+        name: 'Vicky',
+    };
+
+    const name = user.name;
+
+    return username + name;
 }
 
 login('tsingh');

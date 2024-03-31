84bd3fefbda25f986a83021f377349a3ba89e0fc
BikramThangjam <tbsingh1211@gmail.com>
2024-03-31 20:44:37 +0530

    test lint-staged 3

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 function login(username: string): string {
+    // console.log(username);
     const user = {
         name: 'Vicky',
-    };const name = user['name'];
+    };
+    const name = user.name;
 
     return username + name;
 }

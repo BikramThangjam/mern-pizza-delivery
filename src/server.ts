bb3d7ebd93bc720b3bb35fb0f788febfe5e726fe
BikramThangjam <tbsingh1211@gmail.com>
2024-04-01 22:47:35 +0530

    add logger

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+import app from './app';
+import { Config } from './config';
+import logger from './config/logger';
+
+const startServer = () => {
+    const PORT = Config.PORT;
+
+    try {
+        app.listen(PORT, () =>
+            logger.info(`Listening on PORT ${PORT}`, { test: 1 }),
+        );
+    } catch (err) {
+        if (err instanceof Error) {
+            logger.error(err.message);
+            setTimeout(() => {
+                process.exit(1);
+            }, 1000);
+        }
+    }
+};
+
+startServer();
